6c422e9ea7b908f6c4804ef4f22d3df8a12968ea
Kuba Klin <poczta@kubaklin.pl>
2025-04-23 21:36:21 +0200

    fix: move object outside the ArticleModal

--- src/components/ArticleModal/ArticleModal.jsx
+++ src/components/ArticleModal/ArticleModal.jsx
@@ -1,18 +1,19 @@
 import { Box, Modal, Typography } from '@mui/material';
 import { NewArticleForm } from '../NewArticleForm/NewArticleForm';
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 export const ArticleModal = ({ open, onClose, isEditing, article }) => {
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
 
   return (
     <Modal
