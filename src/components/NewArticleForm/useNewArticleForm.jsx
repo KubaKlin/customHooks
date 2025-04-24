9e8c3acc58edc586e2178be58937b3eaf1a7ef52
Kuba Klin <poczta@kubaklin.pl>
2025-04-24 20:52:02 +0200

    fix: remove unnecessary useeffect

--- src/components/NewArticleForm/useNewArticleForm.jsx
+++ src/components/NewArticleForm/useNewArticleForm.jsx
@@ -1,14 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export function useNewArticleForm(initialTitle = '', initialContent = '') {
   const [title, setTitle] = useState(initialTitle);
   const [content, setContent] = useState(initialContent);
-
-  // Update form values when initial values change
-  useEffect(() => {
-    setTitle(initialTitle);
-    setContent(initialContent);
-  }, [initialTitle, initialContent]);
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -18,9 +12,15 @@ export function useNewArticleForm(initialTitle = '', initialContent = '') {
     setContent(event.target.value);
   };
 
+  const resetForm = () => {
+    setTitle(initialTitle);
+    setContent(initialContent);
+  };
+
   return {
     handleTitleChange,
     handleContentChange,
+    resetForm,
     title,
     content,
   };
